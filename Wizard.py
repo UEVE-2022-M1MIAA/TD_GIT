12904342e09250505c03eb37f59cfef61545168a
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2022-11-17 09:26:38 +0100

    add wizard class

---
+++ Wizard.py
@@ -0,0 +1,10 @@
+import random
+
+class Wizard:
+  def __init__(self):
+    self.degat = random.choice([2, 4])
+    self.luck = 20
+    self.fuite = 10
+    self.prix = 15
+    self.unite = "wizard"
+    
